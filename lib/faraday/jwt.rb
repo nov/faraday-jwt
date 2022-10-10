eb6408eb9f41c35637c50ac87e10e2b793f08e72
nov <nov@matake.jp>
2022-10-10 22:40:52 +0900

    formatting

--- lib/faraday/jwt.rb
+++ lib/faraday/jwt.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-require_relative "jwt/version"
+require_relative 'jwt/version'
 
 module Faraday
   module Jwt
--- lib/faraday/jwt/version.rb
+++ lib/faraday/jwt/version.rb
@@ -2,6 +2,6 @@
 
 module Faraday
   module Jwt
-    VERSION = "0.1.0"
+    VERSION = '0.0.0'
   end
 end
